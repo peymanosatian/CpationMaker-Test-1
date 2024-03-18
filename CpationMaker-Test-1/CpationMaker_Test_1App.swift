9c4adc678edcd5e8a0e2aa11a3b3eaffb60d57cb
Peyman <peyman.osatian@gmail.com>
2024-03-18 22:32:55 +0100

    Initial Commit

---
+++ CpationMaker-Test-1/CpationMaker_Test_1App.swift
@@ -0,0 +1,17 @@
+//
+//  CpationMaker_Test_1App.swift
+//  CpationMaker-Test-1
+//
+//  Created by Peyman Osatian on 2024-03-18.
+//
+
+import SwiftUI
+
+@main
+struct CpationMaker_Test_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
